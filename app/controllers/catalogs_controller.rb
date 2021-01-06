29c69b4dadfee05af781e92c5fab4b3f44613a38
adarvin14 <amdarvin14@gmail.com>
2021-01-06 12:42:57 -0500

    modify catalog controller

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -5,60 +5,37 @@ class CatalogsController < ApplicationController
   # GET /catalogs.json
   def index
     @catalogs = Catalog.all
+    render json: catalogs
   end
 
   # GET /catalogs/1
   # GET /catalogs/1.json
   def show
-  end
-
-  # GET /catalogs/new
-  def new
-    @catalog = Catalog.new
-  end
-
-  # GET /catalogs/1/edit
-  def edit
+    render json: @catalog
   end
 
   # POST /catalogs
-  # POST /catalogs.json
   def create
     @catalog = Catalog.new(catalog_params)
-
-    respond_to do |format|
-      if @catalog.save
-        format.html { redirect_to @catalog, notice: 'Catalog was successfully created.' }
-        format.json { render :show, status: :created, location: @catalog }
-      else
-        format.html { render :new }
-        format.json { render json: @catalog.errors, status: :unprocessable_entity }
-      end
+    if @catalog.save
+      render json: @catalog, status: :created, location: @catalog
+    else
+      render json: @catalog.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /catalogs/1
-  # PATCH/PUT /catalogs/1.json
   def update
-    respond_to do |format|
-      if @catalog.update(catalog_params)
-        format.html { redirect_to @catalog, notice: 'Catalog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @catalog }
-      else
-        format.html { render :edit }
-        format.json { render json: @catalog.errors, status: :unprocessable_entity }
-      end
+    if @catalog.update(catalog_params)
+      render json: @catalog
+    else
+      render json: @catalog.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /catalogs/1
-  # DELETE /catalogs/1.json
   def destroy
     @catalog.destroy
-    respond_to do |format|
-      format.html { redirect_to catalogs_url, notice: 'Catalog was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
