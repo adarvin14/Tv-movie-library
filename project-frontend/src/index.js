ef650f6dc6d6e894e889eac990d68dd843df9f24
adarvin14 <amdarvin14@gmail.com>
2021-01-11 18:08:31 -0500

    render catalog and clear form functions

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,10 +1,22 @@
 const BASE_URL = 'http://localhost:3000'
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {
     document.getElementById('catalog-form').addEventListener('click', displayCreateForm)
     document.getElementById('catalogs').addEventListener('click', getCategories)
     getCategories()
 })
+
+async function renderCatalogs() {
+    document.getElementById("new-catalog-form").innerHTML = ""
+    document.getElementById("new-movie-form").innerHTML = ""
+    const catalogs = await apiService.fetchCatalogs()
+    main.innerHTML = ""
+    catalogs.map(catalog => {
+        const newCatalog = new Catalog(catalog)
+        main.innerHTML += newCatalog.renderCatalog() 
+    })
+    attachClicksCatalog()
+}
 
 function displayCreateForm() {
     let formDiv = document.querySelector("#new-catalog-form")
@@ -18,3 +30,10 @@ function displayCreateForm() {
     formDiv.innerHTML = html
     document.querySelector('form').addEventListener('submit', createCatalog)
 }
+
+function clearForm() {
+    let formDiv = document.querySelector('div#catalog-form')
+    formDiv.innerHTML = ''
+}
+
+
