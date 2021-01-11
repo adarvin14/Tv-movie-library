afe0c7a5a29babfcdbc6c9cf8c849c62e6e51eba
adarvin14 <amdarvin14@gmail.com>
2021-01-11 13:55:42 -0500

    js catalog class wip

--- project-frontend/src/components/Catalog.js
+++ project-frontend/src/components/Catalog.js
@@ -1,9 +1,18 @@
 const BASE_URL = 'http://127.0.0.1:3001/catalogs'
 const MOVIE_URL = 'http://127.0.0.1:3001/movies'
 
-const catalogForm = `
-<label>Catalog Name: </label>
-<input id="catalogName" placeholder="Name"></input>
-<input type="hidden" id="catalogId"></input>
-`
+class Catalog {
+    constructor(data){
+        this.id = data.id
+        this.name = data.name
+    }
 
+    static newCatalogForm(){
+        let newCatalogForm = document.getElementById('catalog-form')
+        newCatalogForm.innerHTML = `
+        <form onsubmit="createCatalgog(); return false;">` +
+        CatalogForm +
+        `<input type="submit" value="Create Catalog" >
+        </form>
+        <br> `  
+    }
