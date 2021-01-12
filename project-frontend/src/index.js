9acc0fe22058ba566df10c9e0b8a9516d8153993
adarvin14 <amdarvin14@gmail.com>
2021-01-11 21:39:02 -0500

    testing pushing changes to git

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,35 +1,18 @@
 const BASE_URL = 'http://localhost:3000'
 
 window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('catalogs').addEventListener('click', getCategories)
-    getCategories()
+    document.getElementById('catalogs').addEventListener('click', getCatalogs)
+    getCatalogs()
+
 })
 
-function getCatalogs() {
-    document.getElementById("new-catalog-form").innerHTML = ""
-    document.getElementById("new-movie-form").innerHTML = ""
-    const catalogs = await apiService.fetchCatalogs()
-    main.innerHTML = ""
-    catalogs.map(catalog => {
-        const newCatalog = new Catalog(catalog)
-        main.innerHTML += newCatalog.renderCatalog() 
-    })
-    attachClicksToLinks()
-}
-
-function attachClicksToLinks() {
-    let catalogs = document.querySelectorAll("li a")
-    catalogs.forEach(catalog => {
-        catalog.addEventListener('click', showcatalog)
-    })
-}
-
-function displayCatalogForm() {
-    let formDiv = document.querySelector("#new-catalog-form")
+function displayCreateCatalogForm() {
+    let formDiv = document.querySelector('div#catalog-form')
     let html = `
+        <br>
         <form>
-            <label>Catalog:</label>
-            <input type="text" id="catalog">
+            <label>Catalog Name:</label>
+            <input type="text" id = "name">
             <input type="submit">
         </form>
     `
@@ -42,27 +25,28 @@ function clearForm() {
     formDiv.innerHTML = ''
 }
 
-function createMovie(e) {
+function createCatalog(e) {
     e.preventDefault()
-    let movie = {
-        title: e.target.querySelector("#title").value
+    let catalog = {
+        name: e.target.querySelector("#name").value
     }
 
     let configObj = {
         method: 'POST',
-        body: JSON.stringify(movie),
+        body: JSON.stringify(catalog),
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     }
-    fetch(BASE_URL + '/movies', configObj)
+    fetch(BASE_URL + '/catalogs', configObj)
     .then(res => res .json()) 
-    .then(movie => {
+    .then(catalog => {
         main.innerHTML += `
         <br>
         <li>
-        <a href="#" data-id="${movie.id}">${movie.title}</a>
+        <a href="#" data-id="${catalog.id}">${catalog.name}</a>
+        - <button class="delete-catalog" data-id="${catalog.id}">Delete List</button>
         </li>
         `
         attachClicksToLinks()
@@ -70,4 +54,47 @@ function createMovie(e) {
         }
     ) 
 }
+
+function getCatalogs() {
+    let main = document.getElementById('main')
+    main.innerHTML = ""
+    fetchCatalogs()
+    .then(catalogs => {
+        main.innerHTML = `
+        <a href="#" id="catalog-form">+Create a Movie Catalog</a>
+        <div id="catalog-form"></div>
+        `
+        catalogs.map( catalog => {
+        main.innerHTML += `
+        <br>
+        <li>
+        <a href="#" data-id="${catalog.id}">${catalog.name}</a>
+        - <button class="delete-catalog" data-id="${catalog.id}">Delete Catalog</button>
+        </li>
+        ` 
+        })
+        attachClicksToLinks()
+        clearForm()
+        document.getElementById('catalog-form').addEventListener('click', displayCreateCatalogForm)
+    })
+    
+}
+
+async function fetchCatalogs() {
+    let res = await fetch(BASE_URL + '/catalogs')
+    let data = await res.json()
+    return data
+}
+
+function attachClicksToLinks() {
+    let catalogs = document.querySelectorAll("li a")
+    catalogs.forEach(catalog => {
+        catalog.addEventListener('click', displayCatalog)
+    })
+    let buttons = document.querySelectorAll(".delete-catalog")
+    buttons.forEach(btn => {
+        btn.addEventListener('click', removeCatalog)
+    })
+}
+
 
