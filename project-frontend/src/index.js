a5148d051703b776b39121de5571d560289d2643
adarvin14 <amdarvin14@gmail.com>
2021-01-12 00:09:24 -0500

    delete assets folder

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'tcp://127.0.0.1:3000'
 
 window.addEventListener("DOMContentLoaded", () => {
     document.getElementById('catalogs').addEventListener('click', getCatalogs)
